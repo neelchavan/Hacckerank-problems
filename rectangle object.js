ec44de82d90fb4f8cb38413db539fddd8a8861ff
neelchavan <neelchavan9@gmail.com>
2020-04-16 12:07:22 +0530

    Create an rectangle object

---
+++ rectangle object.js
@@ -0,0 +1,21 @@
+
+    function Rectangle(a, b) {
+        this.length = a;
+        this.width = b;
+        this.perimeter = ((a * 2) + (b * 2));
+        this.area = a * b;
+        //return length;
+        //return width;
+        //return perimeter
+        return area;
+}
+Rectangle(4,5)
+//console.log(length);
+//console.log(width);
+//console.log(perimeter);
+console.log(area);
+
+
+
+
+
